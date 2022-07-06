61726248219af4c0031790c6133c7778a3828a02
cyrus0w <1015804047@qq.com>
2022-07-06 20:55:19 +0800

    update

--- public_installer/password.sh
+++ public_installer/password.sh
@@ -3,7 +3,7 @@ rootPWD="$6$y0xn3L3IaYfFs9Qn$KZMpMOwuznp4sdIe49DHRW/q5SZNXStoRFMASH9vVeyir03mLio
 opsPWD="$6$U1I/krNjRD3H4pQl$aoIUYCElP5ZU6UOCqcyVvCpyERm2D1a5A0KN4kajKpEbKgOsrFvZN6eAVh8VeUqvmc/WFSbZc4AFqccw2rGO40"  # sha-512
 grubPWD="$1$mWzi29hm$rsEYxvTvd4IKS02nQZVZb/"  # md5
 netconsolePWD="123456"  # text
-netconsoleKEY="https://gitee.com/cyrus0w/ipxe-of-cloud/raw/main/public_installer/authorized_keys"  # authorized_keys 
+netconsoleKEY="http://ipxe.com/public_installer/authorized_keys"  # authorized_keys 
 echo "d-i passwd/root-password-crypted password ${rootPWD}" > /tmp/password.cfg
 echo "di passwd/user-password-crypted password ${opsPWD}" >> /tmp/password.cfg
 echo "d-i grub-installer/password-crypted password ${grubPWD}" >> /tmp/password.cfg
